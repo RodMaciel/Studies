aa139a9c21a83acacb20c453ef35f88b1fae8b54
Rodrigo Maciel <rodrigodmc9@gmail.com>
2023-06-24 14:17:33 -0300

    refatorando class pra function components

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import style from "./Botao.module.scss";
 
-interface BotaoProps {
-  children: React.ReactNode;
-  type?: "button" | "submit" | "reset" | undefined, onClick?: () => void
+interface Props {
+  type?: "button" | "submit" | "reset" | undefined;
+  onClick?: () => void,
+  children?: React.ReactNode
 }
 
-class Botao extends React.Component<BotaoProps> {
-  render() {
-    const { type = "button", onClick } = this.props;
-
-    return (
-      <button onClick={onClick} type={type} className={style.botao}>
-        {this.props.children}
-      </button>
-    );
-  }
+function Botao ( {onClick, type, children }: Props) {
+  return (
+    <button 
+    onClick={onClick} type={type} 
+    className={style.botao}>
+      {children}
+    </button>
+  );
 }
 
 export default Botao;
